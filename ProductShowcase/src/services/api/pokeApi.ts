39a1e6ab26519a639a9f4d8b865db35a4c518d98
lucasronan33 <164685834+lucasronan33@users.noreply.github.com>
2025-12-03 20:26:05 -0300

    serviço de API com axios

--- ProductShowcase/src/services/api/pokeApi.ts
+++ ProductShowcase/src/services/api/pokeApi.ts
@@ -1,21 +1,5 @@
 import axios from 'axios';
 
-const API_URL = 'https://pokeapi.co/api/v2';
-
-export const fetchPokemons = async (limit = 20, offset = 0) => {
-    try {
-        const response = await axios.get(`${API_URL}/pokemon?limit=${limit}&offset=${offset}`);
-        return response.data;
-    } catch (error) {
-        throw new Error('Error fetching Pokémon data');
-    }
-};
-
-export const fetchPokemonDetails = async (pokemonName) => {
-    try {
-        const response = await axios.get(`${API_URL}/pokemon/${pokemonName}`);
-        return response.data;
-    } catch (error) {
-        throw new Error('Error fetching Pokémon details');
-    }
-};
+export const api = axios.create({
+    baseURL: 'https://pokeapi.co/api/v2'
+})
