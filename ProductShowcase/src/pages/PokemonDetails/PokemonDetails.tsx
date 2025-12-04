60312d1ed152e8d451be808167f612aac30135f1
lucasronan33 <164685834+lucasronan33@users.noreply.github.com>
2025-12-03 21:27:10 -0300

    Adiciona lógica para carregar detalhes do Pokémon na página PokemonDetails

--- ProductShowcase/src/pages/PokemonDetails/PokemonDetails.tsx
+++ ProductShowcase/src/pages/PokemonDetails/PokemonDetails.tsx
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { PokemonDetails as PokemonDetailsType } from '../../interfaces/Pokemon';
+import { getPokemonDetails } from '../../services/api/pokemonService';
 
 export default function PokemonDetails() {
-  return <div>detalhes</div>;
+  const { name } = useParams<{ name: string }>()
+  
+  const [pokemon, setPokemon] = useState<PokemonDetailsType | null>(null)
+  const [loading, setLoading] = useState(true)
+  
+  useEffect(() => {
+    async function fetchPokemon() {
+      try {
+        if (!name) {
+          const data = await getPokemonDetails(name)
+          setPokemon(data)
+        }
+      } catch (error) {
+        console.error("Erro ao carregar detalhes:", error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchPokemon()
+  }, [name])
+  
 }
--- ProductShowcase/src/services/api/pokemonService.ts
+++ ProductShowcase/src/services/api/pokemonService.ts
@@ -1,8 +1,13 @@
 import React from 'react';
 import { api } from './pokeApi';
-import type { PokemonListResponse } from '../../interfaces/Pokemon';
+import type { PokemonDetails, PokemonListResponse } from '../../interfaces/Pokemon';
 
 export async function getPokemonList(limit = 151) {
     const res = await api.get<PokemonListResponse>(`/pokemon?limit=${limit}`)
+    return res.data
+}
+
+export async function getPokemonDetails(name:string) {
+    const res = await api.get<PokemonDetails>(`/pokemon/${name}`)
     return res.data
 }
